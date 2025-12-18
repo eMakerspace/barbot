1f75935211e801427d44b23e860938a63350f932
Dominik Gschwind <dominik.gschwind99@gmail.com>
2025-12-18 03:19:56 +0100

    Fix emergency stop release logic

--- esp32/barbotv2/src/tasks.rs
+++ esp32/barbotv2/src/tasks.rs
@@ -4,11 +4,11 @@ use futures::{FutureExt, select_biased};
 
 use crate::cmd::{StopCmd, StopCmdImmediatePub};
 
+pub mod lift_motor;
+pub mod pump;
 pub mod route_cmd;
 pub mod serial;
 pub mod stepper;
-pub mod pump;
-pub mod lift_motor;
 
 #[embassy_executor::task]
 pub async fn emergency_stop_monitor(
@@ -16,24 +16,29 @@ pub async fn emergency_stop_monitor(
     stop_pub: StopCmdImmediatePub,
 ) {
     let mut stop_active = false;
+    let mut stop_clearing = false;
     loop {
         select_biased! {
             _ = emergency_stop.wait_for_any_edge().fuse() => (),
-            _ = Timer::after_millis(10).fuse() => (),
+            _ = Timer::after_millis(if stop_clearing { 10 } else { 1000 }).fuse() => (),
         }
 
         let level = emergency_stop.level();
 
         match level {
-            Level::Low if !stop_active => {
+            Level::Low if !stop_active || stop_clearing => {
                 stop_pub.publish_immediate(StopCmd::Immediate);
                 stop_active = true;
+                stop_clearing = false;
                 log::warn!("Emergency stop!\r");
             }
             Level::High if stop_active => {
                 if let Ok(_) = stop_pub.try_publish(StopCmd::Continue) {
                     stop_active = false;
+                    stop_clearing = false;
                     log::info!("Emergency stop cleared\r");
+                } else {
+                    stop_clearing = true;
                 }
             }
             _ => (),
